740937bb945a1dd0ef9f9480254633ec76b39f66
serov.sd39 <sergo.sergej.05@yandex.ru>
2022-03-22 17:54:59 +0500

    first commit 2

---
+++ data/test.py
@@ -0,0 +1,18 @@
+from requests import get, post, delete
+
+print(post('http://localhost:5000/api/news').json())
+
+print(post('http://localhost:5000/api/news',
+           json={'title': 'Заголовок'}).json())
+
+print(post('http://localhost:5000/api/news',
+           json={'title': 'Заголовок',
+                 'content': 'Текст новости',
+                 'user_id': 1,
+                 'is_private': False}).json())
+
+print(delete('http://localhost:5000/api/news/999').json())
+# новости с id = 999 нет в базе
+
+print(delete('http://localhost:5000/api/news/1').json())
+print(delete('http://localhost:5000/api/v2/news/1').json())
